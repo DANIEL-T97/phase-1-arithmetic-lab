a0d238867b766eafa3a8bacf96b5a6308a9f6ec0
Daniel <daniel.mwangi@student.moringaschool.com>
2023-09-21 21:02:44 +0300

    Committed arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31
+const multiply = num1*num2;
+console.log(multiply);
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+const num3 = 32;
+const num4 = 7;
+const mod = num3 % num4;
+console.log(mod);
+const values = [3,4,7,-12,20,19,15,14,11];
+const max = Math.max(...values);
+console.log(max);
